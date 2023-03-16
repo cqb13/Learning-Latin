e4bcdee1f0932fb0bb26d161df68b1b0d4cadc60
cqb13 <74616162+cqb13@users.noreply.github.com>
2023-03-16 16:14:08 -0400

    🖋 Name not changing

--- lib/types/TextProps.ts
+++
@@ -1,9 +0,0 @@
-type textProps = {
-    placeholder: string | string[];
-    value?: any;
-    id?: string | string[];
-    class?: string;
-    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  };
-  
-  export default textProps;
