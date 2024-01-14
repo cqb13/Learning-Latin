5c375c0c85010d10a8703f7efea14955b4133de3
cqb13 <cqb13.main@gmail.com>
2024-01-14 13:57:11 -0500

    improved translation

--- components/translate/translationCard.tsx
+++ components/translate/translationCard.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 
 const TranslationCard = (props: any) => {
   const [isMinimized, setIsMinimized] = useState<boolean>(false);
+  const [moreInfo, setMoreInfo] = useState<boolean>(false);
 
   const changeVisibility = () => {
     setIsMinimized(!isMinimized);
@@ -14,45 +15,91 @@ const TranslationCard = (props: any) => {
     props.removeCard();
   };
 
-  //const data = () => {
-  //  console.log(props.data);
-  //};
+  const create_form_line = (word: any) => {
+    if (word.pos == "noun") {
+      return `${word.form.declension} ${word.form.gender} ${word.pos}`;
+    } else if (word.pos == "verb" || word.pos == "participle") {
+      return `${word.form.kind} ${word.pos}`;
+    } else if (word.pos == "adjective") {
+      return `${word.form.declension} ${word.pos}`;
+    } else {
+      return `${word.pos}`;
+    }
+  };
 
-  //data();
+  const create_inflection_line = (inflectionInfo: any, stemInfo: any) => {
+    const stem = stemInfo.orth;
+    const ending = inflectionInfo.ending;
+    const inflectionForm = inflectionInfo.form;
 
-  //!!!: On mobile icons on card control buttons are not visible
+    const declension = inflectionForm.declension;
+    const number = inflectionForm.number;
+    const gender = inflectionForm.gender;
+    const tense = inflectionForm.tense;
+    const voice = inflectionForm.voice;
+    const mood = inflectionForm.mood;
+    const verb = inflectionForm.verb;
+    const kind = inflectionForm.kind;
+
+    if (
+      declension == "unknown" ||
+      number == "unknown" ||
+      gender == "unknown" ||
+      tense == "unknown" ||
+      voice == "unknown" ||
+      mood == "unknown" ||
+      verb == "unknown" ||
+      kind == "unknown"
+    ) {
+      return "";
+    }
+
+    return `${stem}.${ending} | ${number} ${inflectionInfo.pos} ${declension} ${gender} ${tense} ${voice} ${mood} ${verb} ${kind}`;
+  };
+
   return (
-    <section className="bg-slate-50 bg-opacity-10 rounded flex flex-col p-4 shadow-card" key={props.data.word}>
-      <section className="flex justify-between mb-3">
-        <h2 className="text-3xl font-bold">
-          {props.data.word}
-        </h2>
-        <div className="flex gap-3">
+    <section
+      className='bg-slate-50 bg-opacity-10 rounded flex flex-col p-4 shadow-card'
+      key={props.data.word}
+    >
+      <section className='flex justify-between mb-3'>
+        <div>
+          <h2 className='text-3xl font-bold'>{props.data.word}</h2>
+          <button
+            className='text-primary-color hover:underline active:text-primary-color-dark'
+            onClick={() => setMoreInfo(!moreInfo)}
+          >
+            {moreInfo ? "less info" : "more info"}
+          </button>
+        </div>
+        <div className='flex gap-3'>
           <ToolTip
             content={isMinimized ? "expand card" : "shrink card"}
             delay={20}
           >
-            <Button onClick={changeVisibility} class="h-full">
-              {isMinimized
-                ? <Image
-                    src="/plus.svg"
-                    alt="expand card"
-                    width={20}
-                    height={20}
-                  />
-                : <Image
-                    src="/minus.svg"
-                    alt="shrink card"
-                    width={20}
-                    height={20}
-                  />}
+            <Button onClick={changeVisibility} class='h-full'>
+              {isMinimized ? (
+                <Image
+                  src='/plus.svg'
+                  alt='expand card'
+                  width={20}
+                  height={20}
+                />
+              ) : (
+                <Image
+                  src='/minus.svg'
+                  alt='shrink card'
+                  width={20}
+                  height={20}
+                />
+              )}
             </Button>
           </ToolTip>
           <ToolTip content={"remove card"} delay={20}>
-            <Button onClick={deleteCard} class="h-full">
+            <Button onClick={deleteCard} class='h-full'>
               <Image
-                src="/clear.svg"
-                alt="remove card"
+                src='/clear.svg'
+                alt='remove card'
                 width={20}
                 height={20}
               />
@@ -61,64 +108,133 @@ const TranslationCard = (props: any) => {
         </div>
       </section>
       <section className={`${isMinimized ? "hidden" : ""} flex flex-col gap-3`}>
-        {props.data.defs.length > 0 ? (
+        {props.data.definitions.length > 0 ? (
           <>
-            {props.data.defs.map((def: any) => {
-              return (
-                <div className="bg-slate-50 bg-opacity-25 rounded shadow-card p-2">
-                  <div className="flex gap-3 text-2xl font-semibold">
-                    {def.orth.map((orth: string) => {
-                      return (
-                        <p>
-                          {orth}
-                        </p>
-                      )
-                    })}
-                  </div>
-                  <p>{def.info.gender != "unknown"? def.info.gender : ""} {def.info.pos != "POS"? def.info.pos : ""}</p>
-                  {def.infls ? (
-                    <div className="flex flex-col gap-2 my-3 w-fit">
-                      {def.infls.map((infl: any) => {
-                        return (
-                          <span className="bg-opacity-10 rounded px-2">
-                            {infl.stem}{infl.ending? `.${infl.ending}` : ""}
-                            {infl.stem? ` | ` : ""}
-                            {" "}{infl.form.number && infl.form.number != "unknown"? ` ${infl.form.number} ` : ""}
-                            {infl.pos} 
-                            {infl.form.declension && infl.form.declension != "unknown"? ` ${infl.form.declension}` : ""}
-                            {infl.form.gender && infl.form.gender != "unknown"? ` ${infl.form.gender}` : ""}
-                            {infl.form.tense && infl.form.tense != "unknown"? ` ${infl.form.tense}` : ""}
-                            {infl.form.voice && infl.form.voice != "unknown"? ` ${infl.form.voice}` : ""}
-                            {infl.form.mood && infl.form.mood != "unknown"? ` ${infl.form.mood}` : ""}
-                            {infl.form.person && infl.form.person != "unknown"? ` ${infl.form.person}` : ""}
-                          </span>
-                        )
-                      })}
+            {props.data.definitions.map((definition: any) => (
+              <div className='bg-slate-50 bg-opacity-25 rounded shadow-card p-2'>
+                {definition.word ? (
+                  <>
+                    <div className='flex gap-3 text-2xl font-semibold'>
+                      {definition.word.parts ? (
+                        <>
+                          {definition.word.parts.map((part: any) => (
+                            <span>{part}</span>
+                          ))}
+                        </>
+                      ) : (
+                        <>
+                          {definition.translation.parts.map((part: any) => (
+                            <span>{part}</span>
+                          ))}
+                        </>
+                      )}
+                      {moreInfo ? (
+                        <span className='text-primary-color'>
+                          word id:{" "}
+                          {definition.word.id
+                            ? definition.word.id
+                            : definition.word.wid}
+                        </span>
+                      ) : null}
                     </div>
-                      
-                  ) : (
-                    null
-                  )}
-                  <div className="flex flex-wrap gap-3">
-                  {def.senses.map((sense: string) => {
-                    return (
-                      <p className=" bg-primary-color bg-opacity-10 rounded px-2">
-                        {sense}
-                      </p>
-                    )
-                  })}
+                  </>
+                ) : null}
+                {moreInfo ? (
+                  <>
+                    <div>
+                      {definition.tricks ? (
+                        <div className='flex flex-wrap gap-2'>
+                          <p>Tricks</p>
+                          {definition.tricks.map((trick: any) => (
+                            <p className='bg-primary-color bg-opacity-10 rounded px-2'>
+                              {trick}
+                            </p>
+                          ))}
+                        </div>
+                      ) : null}
+                    </div>
+                    <div className='pl-2 py-2'>
+                      {definition.word.info ? (
+                        <>
+                          <p>Age: {definition.word.info.age}</p>
+                          <p>Area: {definition.word.info.age}</p>
+                          <p>Origin: {definition.word.info.age}</p>
+                          <p>Frequency: {definition.word.info.age}</p>
+                          <p>Source: {definition.word.info.age}</p>
+                        </>
+                      ) : (
+                        <>
+                          <p>Age: {definition.translation.info.age}</p>
+                          <p>Area: {definition.translation.info.age}</p>
+                          <p>Origin: {definition.translation.info.age}</p>
+                          <p>Frequency: {definition.translation.info.age}</p>
+                          <p>Source: {definition.translation.info.age}</p>
+                        </>
+                      )}
+                    </div>
+                  </>
+                ) : null}
+                {definition.word.form ? (
+                  <p>{create_form_line(definition.word)}</p>
+                ) : (
+                  <p>{create_form_line(definition.translation)}</p>
+                )}
+                {definition.inflections ? (
+                  <div className='flex flex-col gap-2 my-3 w-fit'>
+                    {definition.inflections.map((inflection: any) => (
+                      <>
+                        {create_inflection_line(inflection, definition.stem) ==
+                        "" ? null : (
+                          <span className='bg-opacity-10 rounded px-2'>
+                            {create_inflection_line(
+                              inflection,
+                              definition.stem
+                            )}
+                          </span>
+                        )}
+                      </>
+                    ))}
                   </div>
+                ) : null}
+                <div className='flex flex-wrap gap-3'>
+                  {definition.word.senses ? (
+                    <>
+                      {definition.word.senses.map((sense: any) => (
+                        <p className=' bg-primary-color bg-opacity-10 rounded px-2'>
+                          {sense}
+                        </p>
+                      ))}
+                      {definition.word.extension_senses.map((sense: any) => (
+                        <p className=' bg-primary-color bg-opacity-10 rounded px-2'>
+                          {sense}
+                        </p>
+                      ))}
+                    </>
+                  ) : null}
+                  {definition.translation.senses ? (
+                    <>
+                      {definition.translation.senses.map((sense: any) => (
+                        <p className=' bg-primary-color bg-opacity-10 rounded px-2'>
+                          {sense}
+                        </p>
+                      ))}
+                    </>
+                  ) : null}
                 </div>
-              );
-            })
-            }
+              </div>
+            ))}
           </>
         ) : (
           <div>
             <p>Sorry, your word was not found</p>
             <p>Please double check spelling, confirm words existence</p>
-            <p>You believe that program should have found you word, please open in issue in our github repo</p>
-            <a href="https://github.com/Templar-Development/Open-Words-TS/issues/new/choose">Open Words TS</a>
+            <p>
+              You believe that program should have found you word, please open
+              in issue in our github repo
+            </p>
+            <a href='https://github.com/cqb13/vocab-vault/issues/new/choose'>
+              Vocab Vault
+            </a>
           </div>
         )}
       </section>
--- pages/translate.tsx
+++ pages/translate.tsx
@@ -1,91 +1,82 @@
 import TranslationCard from "@components/translate/translationCard";
-import removeExtraSpaces from "@utils/removeExtraSpaces";
-import macronHandler from "@utils/macronHandler";
 import Layout from "@components/shared/layout";
 import Button from "@components/shared/button";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Text from "@components/shared/text";
 import { NextPage } from "next";
 
 const Translate: NextPage = () => {
   const [translations, setTranslations] = useState<any[]>([]);
-  const [query, setQuery] = useState("cur");
-
-  useEffect(() => {
-    getTranslation("latin-to-english", false);
-  }, []);
+  const [query, setQuery] = useState("");
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
   };
 
   const getTranslation = async (
-    type: "latin-to-english" | "english-to-latin",
-    set: boolean
+    type: "latin-to-english" | "english-to-latin"
   ) => {
-    setQuery("");
-
-    //should increase performance
-    if (translations.find(t => t.word === query)) {
-      setTranslations(translations.filter(t => t.word === query));
-      return;
-    }
-
     try {
-      let cleanQuery = macronHandler(query);
-      //if there are extra spaces, empty cards will be created
-      cleanQuery = removeExtraSpaces(cleanQuery);
-      const response = await fetch(
-        `/api/translate?word=${cleanQuery}&lang=${type}`
-      );
-      const data = await response.json();
-      if (!set) return;
-      setTranslations(data);
+      const url =
+        type === "latin-to-english"
+          ? `https://translator.learninglatin.net/latin_to_english?text=${query}&max_definitions=6&use_tricks=true&format_output=true&clean_output=false&sort_output=true&filter_uncommon_translations=true`
+          : `https://translator.learninglatin.net/english_to_latin?text=${query}&max_definitions=6&format_output=true&clean_output=false&sort_output=true`;
+
+      let result = await fetch(url).then((res) => res.json());
+
+      if (result.error) {
+        throw new Error(result.error);
+      } else {
+        console.log(result.translations);
+        setTranslations(result);
+      }
+      setQuery("");
     } catch (error) {
       console.error(error);
     }
   };
 
   const removeCard = (word: string) => {
-    setTranslations(translations.filter(t => t.word !== word));
+    setTranslations(translations.filter((t) => t.word !== word));
   };
 
   return (
-    <Layout title="Translator" backgroundClass="bg-translate-gradient">
-      <section className="flex flex-col items-center">
-        <h1 className="text-5xl text-zinc-800 font-bold m-0 [text-shadow:0_1px_1px_rgba(0,0,0,0.2)]">
+    <Layout title='Translator' backgroundClass='bg-translate-gradient'>
+      <section className='flex flex-col items-center'>
+        <h1 className='text-5xl text-zinc-800 font-bold m-0 [text-shadow:0_1px_1px_rgba(0,0,0,0.2)]'>
           Translator
         </h1>
-        <section className="flex flex-col w-3/5 mt-2 max-xs:w-4/5">
+        <section className='flex flex-col w-3/5 mt-2 max-xs:w-4/5'>
           <Text
-            placeholder="Enter a word"
+            placeholder='Enter a word'
             onChange={onChange}
-            class=" bg-white bg-opacity-30 backdrop-blur-sm"
-            keyName="Enter"
+            class=' bg-white bg-opacity-30 backdrop-blur-sm'
+            keyName='Enter'
             value={query}
           />
-          <div className="flex gap-2 mt-2 max-sm:flex-col">
+          <div className='flex gap-2 mt-2 max-sm:flex-col'>
             <Button
-              onClick={() => getTranslation("latin-to-english", true)}
-              class="w-full"
+              onClick={() => getTranslation("latin-to-english")}
+              class='w-full'
             >
               Latin To English
             </Button>
             <Button
-              onClick={() => getTranslation("english-to-latin", true)}
-              class="w-full"
+              onClick={() => getTranslation("english-to-latin")}
+              class='w-full'
             >
               English To Latin
             </Button>
           </div>
         </section>
 
-        {translations.length > 0 &&
-          <section className="flex flex-col justify-center gap-3 w-3/5 mt-3 max-xs:w-4/5">
-            {translations.map(t =>
+        {translations.length > 0 && (
+          <section className='flex flex-col justify-center gap-3 w-3/5 mt-3 max-xs:w-4/5'>
+            {translations.map((t) => (
               <TranslationCard data={t} removeCard={() => removeCard(t.word)} />
-            )}
-          </section>}
+            ))}
+          </section>
+        )}
       </section>
     </Layout>
   );
