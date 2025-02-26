bb265578b1c7bf8f9fda25e4160ca493ef1f00db
cqb13 <cqb13.main@gmail.com>
2025-02-26 12:36:06 -0500

    make correct chart load when going backwards and forwards

--- app/practice/layout.tsx
+++ app/practice/layout.tsx
@@ -19,39 +19,48 @@ export default function WordLadderLayoutWrapper({
   );
 }
 
-//TODO: make this work when going back browser
 function WordLadderLayout({ children }: { children: any }) {
   const { updateCurrentChart } = usePracticeChartContext() as {
     updateCurrentChart: (chart: PracticeCharts) => void;
   };
 
   useEffect(() => {
-    const url = new URL(window.location.href);
-    if (!url.toString().includes("chart")) {
-      return;
-    }
-    let type = url.searchParams.get("type");
+    const updateChartFromUrl = () => {
+      const url = new URL(window.location.href);
+      if (!url.toString().includes("chart")) {
+        return;
+      }
+      let type = url.searchParams.get("type");
 
-    switch (type) {
-      case "declension-endings":
-        updateCurrentChart(PracticeCharts.DeclensionEndings);
-        break;
-      case "future-tense":
-        updateCurrentChart(PracticeCharts.FutureTense);
-        break;
-      case "perfect-tense":
-        updateCurrentChart(PracticeCharts.PerfectTense);
-        break;
-      case "personal-endings":
-        updateCurrentChart(PracticeCharts.PersonalEndings);
-        break;
-      case "personal-pronouns":
-        updateCurrentChart(PracticeCharts.PersonalPronouns);
-        break;
-      case "relative-pronouns":
-        updateCurrentChart(PracticeCharts.RelativePronouns);
-        break;
-    }
+      switch (type) {
+        case "declension-endings":
+          updateCurrentChart(PracticeCharts.DeclensionEndings);
+          break;
+        case "future-tense":
+          updateCurrentChart(PracticeCharts.FutureTense);
+          break;
+        case "perfect-tense":
+          updateCurrentChart(PracticeCharts.PerfectTense);
+          break;
+        case "personal-endings":
+          updateCurrentChart(PracticeCharts.PersonalEndings);
+          break;
+        case "personal-pronouns":
+          updateCurrentChart(PracticeCharts.PersonalPronouns);
+          break;
+        case "relative-pronouns":
+          updateCurrentChart(PracticeCharts.RelativePronouns);
+          break;
+        default:
+          updateCurrentChart(PracticeCharts.DeclensionEndings);
+          break;
+      }
+    };
+
+    updateChartFromUrl(); // Initial check
+
+    window.addEventListener("popstate", updateChartFromUrl);
+    return () => window.removeEventListener("popstate", updateChartFromUrl);
   }, []);
 
   return children;
